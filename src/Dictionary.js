0b9524e959aa0f22ac9ec1d0e0aee2d4e59ab2a8
Diana L Munoz <dlmp7895@gmail.com>
2021-06-05 17:55:06 -0500

    keyword is defined

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -2,21 +2,24 @@ import React, { useState } from "react";
 import "./Dictionary.css";
 
 export default function Dictionary() {
-    let [keyword, setKeyword] = useState("null");
-
-    function search(event){
-        event.preventDefault();
-    }
-      function handleKeyword(event) {
+    let [keyword, setKeyword] = useState("");
+    
+    function handleKeyword(event) {
         console.log(event);
         setKeyword(event.target.value);
       }
+
+    function search(event){
+        event.preventDefault();
+        alert(`Searching for ${keyword} definition`);
+    }
+      
 
     return (
         <div className="Dictionary">
             <form onSubmit={search}>
                 <input type="search" onChange={handleKeyword} />
-                <a href="#" className="btn btn-primary">Search</a>
+    
             </form>
         </div>
     );
