1ad6b1b1372a1e1d4392c6b1b274eaf72596c98b
Diana L Munoz <dlmp7895@gmail.com>
2021-06-05 15:36:22 -0500

    Dictionary web page was added

--- src/App.js
+++ src/App.js
@@ -1,15 +1,24 @@
 import './App.css';
+import Dictionary from "./Dictionary";
 
 export default function App() {
   return (
     <div className="App">
+      <div className="container">
       <header className="App-header">
       
         <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/7/7d/Eo_circle_blue_letter-d.svg/1024px-Eo_circle_blue_letter-d.svg.png" alt="dictionary" className="DictionaryImg" />
           <input placeholder="Type the word"></input>
           <a href="#" className="btn btn-primary">Search</a>
-          
+        
       </header>
+      <main>
+        <Dictionary />
+      </main>
+      <footer className="text-center">
+        Coded by Diana L. Muñoz
+      </footer>
+      </div>
     </div>
   );
 }
