5bfc0052732dea075f32f801c18ff30e5e7c995d
Diana L Munoz <dlmp7895@gmail.com>
2021-06-05 15:13:21 -0500

    input and button was added

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ export default function App() {
       <header className="App-header">
       
         <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/7/7d/Eo_circle_blue_letter-d.svg/1024px-Eo_circle_blue_letter-d.svg.png" alt="dictionary" className="DictionaryImg" />
+         <input placeholder="Type the word here"></input>
+          <a href="#" className="btn btn-primary">Search</a>
           
       </header>
     </div>
