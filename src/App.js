a92852a291fd7381d16d91abd8efafd70dabb5f8
Diana L Munoz <dlmp7895@gmail.com>
2021-06-05 15:27:29 -0500

    Updated dictionary app

--- src/App.js
+++ src/App.js
@@ -6,12 +6,10 @@ export default function App() {
       <header className="App-header">
       
         <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/7/7d/Eo_circle_blue_letter-d.svg/1024px-Eo_circle_blue_letter-d.svg.png" alt="dictionary" className="DictionaryImg" />
-         <input placeholder="Type the word"></input>
+          <input placeholder="Type the word"></input>
           <a href="#" className="btn btn-primary">Search</a>
           
       </header>
     </div>
   );
 }
-
-
