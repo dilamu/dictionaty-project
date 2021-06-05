e63f83f02194de92020d8968444d8cefabaf2295
Diana L Munoz <dlmp7895@gmail.com>
2021-06-05 16:22:19 -0500

    the dictionary function was added for future event

--- src/App.js
+++ src/App.js
@@ -8,15 +8,14 @@ export default function App() {
       <header className="App-header">
       
         <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/7/7d/Eo_circle_blue_letter-d.svg/1024px-Eo_circle_blue_letter-d.svg.png" alt="dictionary" className="DictionaryImg" />
-          <input placeholder="Type the word"></input>
-          <a href="#" className="btn btn-primary">Search</a>
+          
         
       </header>
       <main>
         <Dictionary />
       </main>
-      <footer className="text-center">
-        Coded by Diana L. Muñoz
+      <footer className="App-footer">
+        <small>Coded by Diana L. Muñoz</small>
       </footer>
       </div>
     </div>
--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,5 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
+import "./Dictionary.css";
 
 export default function Dictionary() {
-    return "Hellow from Dictionary";
+    let [keyword, setKeyword] = useState("null");
+
+    function search(event){
+        event.preventDefault();
+    }
+      function handleKeyword(event) {
+        console.log(event);
+        setKeyword(event.target.value);
+      }
+
+    return (
+        <div className="Dictionary">
+            <form onSubmit={search}>
+                <input type="search" onChange={handleKeyword} />
+                <a href="#" className="btn btn-primary">Search</a>
+            </form>
+        </div>
+    );
 }
