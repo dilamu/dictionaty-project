016a1c74053c2ebd5f3eeed0eff819d7c57a747a
Diana L Munoz <dlmp7895@gmail.com>
2021-06-05 15:03:54 -0500

    initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+export default function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      
+        <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/7/7d/Eo_circle_blue_letter-d.svg/1024px-Eo_circle_blue_letter-d.svg.png" alt="dictionary" className="DictionaryImg" />
+          
+      </header>
+    </div>
+  );
+}
+
+
