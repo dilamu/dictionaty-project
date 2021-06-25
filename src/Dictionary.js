64852bb1c0f9f249d5eec8abf5c93fc0450a3318
Diana L Munoz <dlmp7895@gmail.com>
2021-06-25 13:23:07 -0500

    Pexels images were requested

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -2,19 +2,31 @@ import React, { useState } from "react";
 import axios from "axios";
 import "./Dictionary.css";
 import Define from "./Define";
+import Photos from "./Photos";
 
 
 export default function Dictionary() {
     let [keyword, setKeyword] = useState("dictionary");
     let [define, setDefine] = useState(null);
     let [loaded, setLoaded] = useState(false);
+    let [photos, setPhotos] = useState(null);
     
     function search() {
         //https://dictionaryapi.dev/
         let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en_US/${keyword}`;
         axios.get(apiUrl).then(handleResponse);
-    }
     
+        let pexelsApiKey = "563492ad6f91700001000001e35650279cff4670bae9d0650dcb780f";
+        let pexelsApiUrl = `https://api.pexels.com/v1/search?query=${keyword}&per_page=8`;
+        
+        let headers = { Authorization: `Bearer ${pexelsApiKey}`};
+        axios.get(pexelsApiUrl, {headers: headers }).then(handlePexelsResponse);
+    }
+        
+    function handlePexelsResponse(response) {
+        setPhotos(response.data.photos);
+    }
+
 
     function handleSubmit(event) {
         event.preventDefault();
@@ -38,11 +50,13 @@ export default function Dictionary() {
     return (
         <div className="Dictionary">
             <section>
+                <h2 className="word"> Search for word</h2>
                 <form onSubmit={handleSubmit}>
                     <input type="search" onChange={handleKeywordChange} />
                 </form>
             </section>
             <Define define={define} />
+            <Photos photos={photos} />
         </div>
     );  
     } else {
